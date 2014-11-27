bd902f7d5c4b9883915d80f5264db7175230845f
Devon Meunier <devon.meunier@gmail.com>
2014-11-27 15:58:46 -0500

    Update setup.py with license and classifiers.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup
 
-setup(name='Thorium',
-      version='0.1.38',
-      description='A Python framework for simple RESTful API interfaces',
-      author='Ryan Easterbrook',
-      author_email='ryan@eventmobi.com',
-      url='https://github.com/EventMobi/thorium',
-      packages=['thorium'],
-      install_requires=['Flask==0.10.1']
-      )
+setup(
+    name='Thorium',
+    version='0.1.38',
+    description='A Python framework for RESTful API interfaces in Flask',
+    author='Ryan Easterbrook',
+    author_email='ryan@eventmobi.com',
+    url='https://github.com/EventMobi/thorium',
+    packages=['thorium'],
+    install_requires=['Flask==0.10.1'],
+    license='BSD',
+    classifiers=[
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ]
+)
