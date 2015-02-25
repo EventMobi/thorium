4940570303766a95911cb72fb470c181e6aa8311
Dmitry Kichenko <dmitry@eventmobi.com>
2015-02-25 16:20:41 -0500

    Add JSON field type, bump version no

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Thorium',
-    version='0.1.45',
+    version='0.1.47',
     description='A Python framework for RESTful API interfaces in Flask',
     author='Ryan Easterbrook',
     author_email='ryan@eventmobi.com',
