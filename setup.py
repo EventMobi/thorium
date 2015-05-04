d28ee71ca25918003eeca60f1ae390e8961e7a94
Devon Meunier <devon.meunier@gmail.com>
2015-05-04 10:35:28 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Thorium',
-    version='0.1.52.1',
+    version='0.1.53',
     description='A Python framework for RESTful API interfaces in Flask',
     author='Ryan Easterbrook',
     author_email='ryan@eventmobi.com',
